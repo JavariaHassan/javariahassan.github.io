9282a4ef61af922412bebc6fb3b166a9a4d7e678
Javaria Hassan <javariahassan98@gmail.com>
2022-03-05 20:40:26 -0500

    cv and footer updates

---
+++ static/js/footer.js
@@ -0,0 +1,11 @@
+footer = "\
+  <div class='profile-footer profile_theme_two'>\
+    <div class='ufo--container'>\
+      <div class='profile-footer-content'>\
+        <a href='mailto:javariahassan98@gmail.com' target='_blank' rel='nofollow noopener' class='footer-url'>email</a>\
+        <a href='https://www.linkedin.com/in/javariahassan/' target='_blank' rel='nofollow noopener' class='footer-url'>linkedin</a>\
+        <a href='https://github.com/JavariaHassan' target='_blank' rel='nofollow noopener' class='footer-url'>github</a>\
+      </div>\
+    </div>\
+  </div>"
+document.getElementById('footer').innerHTML = footer;
