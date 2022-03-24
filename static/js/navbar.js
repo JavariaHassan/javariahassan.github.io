4edd152058e58333a4ef32ea7acd083970572c20
Javaria Hassan <javariahassan98@gmail.com>
2022-03-24 19:17:45 -0400

    add navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,25 @@
+const toggle = document.getElementsByClassName("ufo--page-navbar-toggle")[0];
+const toggle_container = document.getElementsByClassName("ufo--page-navbar-container")[0];
+toggle.onclick = function() {
+  if (toggle.classList.contains("on")) {
+    toggle.classList.remove("on");
+    toggle_container.classList.remove("on");
+  } else {
+    toggle.classList.add('on');
+    toggle_container.classList.add('on');
+  }
+}
+
+const navbar_items = document.getElementsByClassName("ufo--page-navbar-item");
+Array.from(navbar_items).forEach(item => {
+  console.log('hey', item.getElementsByTagName('div'));
+  try {
+    item_child = item.getElementsByTagName('div')[0];
+  } catch {
+    item_child = item.getElementsByTagName('a')[0];
+  }
+  item_child.addEventListener("click", function() {
+    toggle.classList.remove("on");
+    toggle_container.classList.remove("on");
+  })
+});
